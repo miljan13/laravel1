44824b1160a94c7607d5cc1cda3c40046503a137
Miljan Milivojevic <miljan.milivojevic2@gmail.com>
2023-05-10 21:37:47 +0200

    prepravljena migracija types

--- laravel/database/migrations/2023_05_10_193334_types_table.php
+++ laravel/database/migrations/2023_05_10_193334_types_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('name')->unique();
             $table->timestamps();
         });
